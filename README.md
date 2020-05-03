965f4c999181f46931e650fb62199b518f9903b2
Bruno Rodrigues <brodrigues1990@gmail.com>
2020-05-02 23:20:40 -0300

    create frontend reactjs

---
+++ README.md
@@ -0,0 +1,1 @@
+Desafio Editora Globo
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      Primeiro commit
+    </>
+  );
+}
+
+export default App;
